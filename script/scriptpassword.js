417f005a104705808bf9599ced3b2c3b35cc23b0
titopope@hotmail.com <titopope@hotmail.com>
2023-12-18 11:00:51 +0100

    lios con las contraseñas

---
+++ script/scriptpassword.js
@@ -0,0 +1,6 @@
+const upperCasePass ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCasePass ="abcdefghijklmnopqrstuvwxyz"
+const numberPass ="0123456789"
+const simbolPass ="!@#$%^&*()-_=+"
+let passChar= upperCasePass + lowerCasePass + numberPass + simbolPass;
+
